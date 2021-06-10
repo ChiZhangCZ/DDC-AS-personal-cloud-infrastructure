5aeb59c4a562e93d64dc4c1e09b1f6cd5afbc462
chi zhang <czchizhang@gmail.com>
2021-06-10 15:37:00 +0100

    Basic private bucket

---
+++ bucket.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "vm_bucket" {
+  bucket = "vm-store-${local.timestamp_sanitized}"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket                  = aws_s3_bucket.vm_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
